7c6828d60b6f0120f58115fe304758e7e62370e7
babuchalla <142408957+babuchalla@users.noreply.github.com>
2023-08-17 19:36:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ streamlit.dataframe(fruits_to_show)
 #streamlit.header("The fruit load list contains:")
 #streamlit.dataframe(my_data_rows)
 def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + this_fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
     
