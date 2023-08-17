60e7f85d07cb209debc05c1cf05f04378fb7a96a
babuchalla <142408957+babuchalla@users.noreply.github.com>
2023-08-17 15:20:40 +0530

    Update streamlit_app.py
    normalize data as json data with pandas
    display the fruityvice value as data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize data as json data with pandas
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display the fruityvice value as data frame
+streamlit.dataframe(fruityvice_normalized)
